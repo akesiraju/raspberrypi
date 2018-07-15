84849233c3860f43850b833076646489eb61fe60
Aravind Kesiraju <aravind@analyzere.com>
2018-07-15 18:21:32 -0400

    Setup a web server to control the car over web.

--- car/http_server.py
+++ car/http_server.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from flask import jsonify
+from flask_cors import CORS, cross_origin
 import drive
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+CORS(app)
+
 
 @app.route("/<int:direction_id>")
 def move(direction_id):
     signal = drive.move(direction_id)
-    return jsonify({'Moving':signal})
+    return jsonify({'status':direction_id})
+
+@app.route("/")
+def control():
+    return render_template('index.html', title='Car Control')
+
 
 if __name__ == '__main__':
     app.run(port=5002)
