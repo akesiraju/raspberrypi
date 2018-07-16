9e5b9b21ee829c7c2f89e828fc96bcbb921fe9e1
Aravind Kesiraju <aravind@analyzere.com>
2018-07-15 21:28:06 -0400

    Extract car object from drive.

---
+++ car/car.py
@@ -0,0 +1,186 @@
+# Return the distance to the block infront
+# Libraries
+
+import RPi.GPIO as GPIO
+import time
+#import keyboard
+import sys, tty
+
+
+class Car():
+    def __init__(self):
+        tty.setraw(sys.stdin.fileno())
+
+        self.turntime = 0.2
+        self.drivetime = 0.75
+
+        self.prev_key=0
+        self.brakes = False
+        self.isturning = False
+        self.angle = 100
+        self.turningRadius=20
+        # GPIO Mode (BOARD / BCM)
+        GPIO.setmode(GPIO.BCM)
+        # make right turn by deafault the first time
+        self.left = True
+
+        # ultra sonic controls, trigger and echo
+        self.trigger = 23
+        self.echo = 24
+
+        # servo motor data
+        self.servo = 18
+
+        # motor driver
+        self.input1 = 20
+        self.input2 = 16
+        self.enable1 = 21  # always disable after finishing and\or in catch
+
+        # set GPIO direction (IN / OUT)
+
+        # we send instructions to motor driver IC
+        GPIO.setup(self.input1, GPIO.OUT)
+        GPIO.setup(self.input2, GPIO.OUT)
+        # this will determine if signal should be sent to output pins
+        GPIO.setup(self.enable1, GPIO.OUT)
+
+        GPIO.setup(self.servo, GPIO.OUT)
+        self.pwm = GPIO.PWM(self.servo, 50)
+        self.pwm.start(0)
+
+        GPIO.setup(self.trigger, GPIO.OUT)
+        GPIO.setup(self.echo, GPIO.IN)
+
+    def Log(self, msg):
+        print(msg)
+        #pass
+
+    def distance(self):
+        GPIO.output(self.trigger, GPIO.HIGH)
+        time.sleep(0.00001)
+        GPIO.output(self.trigger, GPIO.LOW)
+        startTime = time.time()
+        stopTime = time.time()
+
+        while GPIO.input(self.echo) == 0:
+            startTime = time.time()
+
+        while GPIO.input(self.echo) == 1:
+            stopTime = time.time()
+
+        elapsed = stopTime - startTime
+        dist = (elapsed * 34300) / 2
+
+        self.Log("distance = " + str(dist))
+
+        return dist
+
+    def getDistance(self):
+        while True:
+            self.distance()
+            time.sleep(1)
+
+    # Set servo motor angle
+    def SetAngle(self, angle):
+        duty = angle / 18 + 2
+        GPIO.output(self.servo, GPIO.HIGH)
+        self.pwm.ChangeDutyCycle(duty)
+        #time.sleep(1.0)
+        GPIO.output(self.servo, GPIO.LOW)
+
+
+    # Turn the servomotor left
+    def TurnLeft(self):
+        self.SetAngle(self.angle + self.turningRadius)
+        self.isturning = True
+        #time.sleep(3.0)
+        #SetAngle(90)
+
+
+    # Turn the servo motor right
+    def TurnRight(self):
+        self. SetAngle(self.angle - self.turningRadius)
+        self.isturning = True
+        #time.sleep(3.0)
+        #SetAngle(90)
+
+
+    # Turn servo motor
+    def Turn(self):
+        self.Log(self.left)
+
+        if self.left == True:
+            self.TurnRight()
+            self.left = False
+        else:
+            self.TurnLeft()
+            self.left = True
+
+
+    # drive forward
+    def  straight(self):
+        if self.isturning == True:
+            self.SetAngle(self.angle)
+            self.isturning = False
+        else:
+            self.Log('no turning')
+
+    def back(self):    
+        self.straight()
+        #Log('move start')
+
+        GPIO.output(self.enable1, GPIO.HIGH)
+        # if input1 and input2 are swapped a regular motor will go in reverse.
+        GPIO.output(self.input1, GPIO.HIGH)
+        # but lego will not as it has different pin for reverse. so we should disable this signal and enable that , creepy
+        GPIO.output(self.input2, GPIO.LOW)
+
+        # time.sleep(2)
+
+        # GPIO.output(enable1, GPIO.LOW)
+
+
+    def front(self):
+        #Log('move start')
+        # if input1 and input2 are swapped we go in reverse.        
+        
+        self.straight()
+        GPIO.output(self.input1, GPIO.LOW)
+        GPIO.output(self.input2, GPIO.HIGH)
+
+        GPIO.output(self.enable1, GPIO.HIGH)
+
+        # time.sleep(2)
+
+        # GPIO.output(enable1, GPIO.LOW)
+    def clear(self):
+        self.pwm.stop()
+        GPIO.output(self.input1, GPIO.LOW)
+        GPIO.output(self.enable1, GPIO.LOW)
+        GPIO.cleanup()
+
+
+    def move(self, signal):
+        if signal == 259:
+            if self.brakes == True:
+                GPIO.output(self.enable1, GPIO.HIGH)
+                self.brakes = False
+            self.front()
+
+        if signal == 260:
+            self.TurnLeft()
+
+        if signal == 261:
+            self.TurnRight()
+
+        if signal == 258:
+            self.back()
+
+        if signal == 32:
+            self.brakes = True
+            GPIO.output(self.enable1, GPIO.LOW)
+
+        if signal == 27:
+            self.clear()
+        
+        return signal
--- car/drive.py
+++ car/drive.py
@@ -1,211 +1,20 @@
-# Return the distance to the block infront
-# Libraries
-from curses import wrapper
-import RPi.GPIO as GPIO
-import time
-#import keyboard
-import sys, tty
+from car import Car
 import threading
+import time
+from curses import wrapper
 
-tty.setraw(sys.stdin.fileno())
+import sys, tty
 
-prev_key=0
-brakes = False
-isturning = False
-angle = 100
-turningRadius=20
-# GPIO Mode (BOARD / BCM)
-GPIO.setmode(GPIO.BCM)
-# make right turn by deafault the first time
-left = True
-
-# ultra sonic controls, trigger and echo
-trigger = 23
-echo = 24
-
-# servo motor data
-servo = 18
-
-# motor driver
-input1 = 20
-input2 = 16
-enable1 = 21  # always disable after finishing and\or in catch
-
-# set GPIO direction (IN / OUT)
-
-# we send instructions to motor driver IC
-GPIO.setup(input1, GPIO.OUT)
-GPIO.setup(input2, GPIO.OUT)
-# this will determine if signal should be sent to output pins
-GPIO.setup(enable1, GPIO.OUT)
-
-GPIO.setup(servo, GPIO.OUT)
-pwm = GPIO.PWM(servo, 50)
-pwm.start(0)
-
-GPIO.setup(trigger, GPIO.OUT)
-GPIO.setup(echo, GPIO.IN)
-
-def Log(msg):
-    print(msg)
-    #pass
-
-def distance():
-    GPIO.output(trigger, GPIO.HIGH)
-    time.sleep(0.00001)
-    GPIO.output(trigger, GPIO.LOW)
-    startTime = time.time()
-    stopTime = time.time()
-
-    while GPIO.input(echo) == 0:
-        startTime = time.time()
-
-    while GPIO.input(echo) == 1:
-        stopTime = time.time()
-
-    elapsed = stopTime - startTime
-    dist = (elapsed * 34300) / 2
-
-    Log("distance = " + str(dist))
-
-    return dist
-
-def getDistance():
-    while True:
-            distance()
-            time.sleep(1)
-
-# Set servo motor angle
-def SetAngle(angle):
-    duty = angle / 18 + 2
-    GPIO.output(servo, GPIO.HIGH)
-    pwm.ChangeDutyCycle(duty)
-    #time.sleep(1.0)
-    GPIO.output(servo, GPIO.LOW)
-
-
-# Turn the servomotor left
-
-
-def TurnLeft():
-    global isturning
-    SetAngle(angle + turningRadius)
-    isturning = True
-    #time.sleep(3.0)
-    #SetAngle(90)
-
-
-# Turn the servo motor right
-
-
-def TurnRight():
-    global isturning
-    SetAngle(angle - turningRadius)
-    isturning = True
-    #time.sleep(3.0)
-    #SetAngle(90)
-
-
-# Turn servo motor
-
-
-def Turn():
-
-    global left
-
-    Log(left)
-
-    if left == True:
-        TurnRight()
-        left = False
-    else:
-        TurnLeft()
-        left = True
-
-
-# drive forward
-def  straight():
-	global isturning
-
-	if isturning == True:
-		SetAngle(angle)
-		isturning = False
-	else:
-		Log('no turning')
-
-def back():
-    
-
-    straight()
-    #Log('move start')
-
-    GPIO.output(enable1, GPIO.HIGH)
-    # if input1 and input2 are swapped a regular motor will go in reverse.
-    GPIO.output(input1, GPIO.HIGH)
-    # but lego will not as it has different pin for reverse. so we should disable this signal and enable that , creepy
-    GPIO.output(input2, GPIO.LOW)
-
-    # time.sleep(2)
-
-    # GPIO.output(enable1, GPIO.LOW)
-
-
-def front():
-    #Log('move start')
-    # if input1 and input2 are swapped we go in reverse.
-	
-    
-    straight()
-    GPIO.output(input1, GPIO.LOW)
-    GPIO.output(input2, GPIO.HIGH)
-
-    GPIO.output(enable1, GPIO.HIGH)
-
-    # time.sleep(2)
-
-
-    # GPIO.output(enable1, GPIO.LOW)
-def clear():
-    pwm.stop()
-    GPIO.output(input1, GPIO.LOW)
-    GPIO.output(enable1, GPIO.LOW)
-    GPIO.cleanup()
-
-
-def move(signal):
-    global brakes
-
-    if signal == 259:
-        if brakes == True:
-            GPIO.output(enable1, GPIO.HIGH)
-            brakes = False
-        front()
-
-    if signal == 260:
-        TurnLeft()
-
-    if signal == 261:
-        TurnRight()
-
-    if signal == 258:
-        back()
-
-    if signal == 32:
-        brakes = True
-        GPIO.output(enable1, GPIO.LOW)
-
-    if signal == 27:
-        clear()
-    
-    return signal
-
-
-def manual(stdscr):
-    global prev_key
+def manual():
+    tty.setraw(sys.stdin.fileno())
     stdscr.clear()
 
-    t = threading.Thread(target = getDistance)
+    my_car = Car()
+    
+
+    t = threading.Thread(target = my_car.getDistance())
     t.daemon = True
+    prev_key = ''
     #t.start()
 
     end_time = time.time() + 300 * 5  #mins
@@ -217,8 +26,7 @@ def manual(stdscr):
             pass
         else:
             prev_key = v
-            Log(v)
-            move(v)
+            my_car.move(v)
             
         if v == 27:
             break
@@ -227,54 +35,5 @@ def manual(stdscr):
     stdscr.refresh()
 
 
-def auto():
-    front()
-
-    end_time = time.time() + 30 * 1  # 60 is 1 minute
-    while time.time() < end_time:
-
-        Log('moving')        
-
-        if distance() < 40:
-            back()
-
-        if distance() < 20:
-            break
-
-        if distance() > 200:
-            front()
-
-        time.sleep(0.5)
-
-    clear()
-
-turntime = 0.2
-drivetime = 0.75
-
-def square():
-    end_time = time.time() + 30 * 1  # 60 is 1 minute
-    while time.time() < end_time:
-        front()
-        time.sleep(drivetime)
-        TurnRight()
-        time.sleep(turntime)
-        front()
-        time.sleep(drivetime)
-        TurnRight()
-        time.sleep(turntime)
-        front()
-        time.sleep(drivetime)
-        TurnRight()
-        time.sleep(turntime)
-        front()
-        time.sleep(drivetime)
-        TurnRight()
-        time.sleep(turntime)
-        break
-
-    clear()
-
-# if __name__ == "__main__":
-    #wrapper(manual)
-    #auto()
-    #square()
+if __name__ == "__main__":
+    wrapper(manual)
