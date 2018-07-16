deb3f9b0d0eb4b2d1114fa4a160a2745e23c4f89
akesiraju <akesiraju@users.noreply.github.com>
2018-07-15 22:24:33 -0400

    Update http_server.py

--- car/http_server.py
+++ car/http_server.py
@@ -1,21 +1,23 @@
 from flask import Flask, request, render_template
 from flask import jsonify
-from flask_cors import CORS, cross_origin
-import drive
+#from flask_cors import CORS, cross_origin
+from car import Car
 
 app = Flask(__name__, static_url_path='')
-CORS(app)
+#CORS(app)
+my_car = Car()
 
 
 @app.route("/<int:direction_id>")
 def move(direction_id):
-    signal = drive.move(direction_id)
+    signal = my_car.move(direction_id)
     return jsonify({'status':direction_id})
 
 @app.route("/")
 def control():
+    my_car = Car()
     return render_template('index.html', title='Car Control')
 
 
 if __name__ == '__main__':
-    app.run(port=5002)
+    app.run(host='192.168.2.18',port=5002)
