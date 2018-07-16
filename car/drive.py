668538397a69e39e8f47b55ce9d9e5aa3c8ca257
akesiraju <akesiraju@users.noreply.github.com>
2018-07-15 22:23:59 -0400

    Update drive.py

--- car/drive.py
+++ car/drive.py
@@ -1,39 +1,18 @@
 from car import Car
-import threading
 import time
-from curses import wrapper
-
-import sys, tty
-
-def manual():
-    tty.setraw(sys.stdin.fileno())
-    stdscr.clear()
-
-    my_car = Car()
-    
-
-    t = threading.Thread(target = my_car.getDistance())
-    t.daemon = True
-    prev_key = ''
-    #t.start()
-
-    end_time = time.time() + 300 * 5  #mins
-    while time.time() < end_time:
-        #time.sleep(0.01)
-        v = stdscr.getch()
-        
-        if prev_key == v:
-            pass
-        else:
-            prev_key = v
-            my_car.move(v)
-            
-        if v == 27:
-            break
-
-    
-    stdscr.refresh()
-
 
 if __name__ == "__main__":
-    wrapper(manual)
+    my_car = Car()
+    
+    # This is a test script.
+    my_car.move(259)
+    time.sleep(3)
+    my_car.move(260)
+    time.sleep(3)
+    my_car.move(261)
+    time.sleep(3)
+    my_car.move(258)
+    time.sleep(3)
+    my_car.move(32)
+    time.sleep(1)
+    my_car.move(27)
