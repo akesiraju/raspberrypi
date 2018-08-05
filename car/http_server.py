d202c5063487c10ab1521503e5e9f2d50a4478e0
= <{ID}+{username}@users.noreply.github.com>
2018-08-05 15:27:15 -0400

    Stream video with usb camera on the car using open CV

--- car/http_server.py
+++ car/http_server.py
@@ -1,6 +1,7 @@
 import socket
 import json
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, Response
+from camera import VideoCamera
 from flask import jsonify
 #from flask_cors import CORS, cross_origin
 import car
@@ -9,6 +10,17 @@ import car_beta
 app = Flask(__name__, static_url_path='')
 # CORS(app)
 my_car = [None] * 1
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route("/<int:signal>")
