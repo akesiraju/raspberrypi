7ae877f8fb197f25cd02cdb51b4cd4cd842b796d
Aravind Kesiraju <akesiraju@users.noreply.github.com>
2020-12-23 16:23:26 -0500

    refactor motionsensor

--- sensors/motionSensor.py
+++ sensors/motionSensor.py
@@ -1,43 +1,44 @@
-import RPi.GPIO as GPIO                           #Import GPIO library
-import time                                       #Import time library
+import RPi.GPIO as GPIO  # Import GPIO library
+import time  # Import time library
 
-#Clockwise increases sensitivity
+# Clockwise increases sensitivity
 
 GPIO.setmode(GPIO.BCM)
 
-#this is where motion sensor is connected
+# this is where motion sensor is connected
 pir = 21
 
-#this is where led is connected to tell us about motion
+# this is where led is connected to tell us about motion
 light = 20
 
-#motion sensor is input
+# motion sensor is input
 GPIO.setup(pir, GPIO.IN)
 
-#led is output
-GPIO.setup(light, GPIO.OUT)
+# led is output
+# GPIO.setup(light, GPIO.OUT)
 
 print("Waiting for sensor to settle")
-time.sleep(2)                                     
+time.sleep(2)
 print("Detecting motion")
 
-#We light up on 20 if there is movement
-while True:
-    if GPIO.input(pir):                            
-        print('movement')
-        #GPIO.output(light, GPIO.HIGH)
-        
-        for i in range(1,10):
-            GPIO.output(light, GPIO.HIGH)
-            time.sleep(0.1)
-            GPIO.output(light, GPIO.LOW)
-            time.sleep(0.1)        
-        
-        #time.sleep(2)                              
-    else:
-        print("no movement")
-        GPIO.output(light, GPIO.LOW)
-    time.sleep(1)   
+# We light up on 20 if there is movement
+try:
+    while True:
+        if GPIO.input(pir):
+            print("movement")
+            # #GPIO.output(light, GPIO.HIGH)
 
-GPIO.cleanup()    
-    
+            # for i in range(1,10):
+            #     GPIO.output(light, GPIO.HIGH)
+            #     time.sleep(0.1)
+            #     GPIO.output(light, GPIO.LOW)
+            #     time.sleep(0.1)
+
+            # #time.sleep(2)
+        else:
+            print("no movement")
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("keyboard interrupt")
+    GPIO.cleanup()
+    exit()
