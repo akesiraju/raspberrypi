59040ebe4c31cb44cbe5da13c336b3abe1b25225
Aravind <akesiraju@users.noreply.github.com>
2022-02-13 14:43:13 -0500

    Show live feed on video_feed endpoint

--- mike/http_server.py
+++ mike/http_server.py
@@ -2,24 +2,24 @@ import socket
 import json
 from flask import Flask, request, render_template, Response
 
-# from camera import VideoCamera
+from camera import VideoCamera
 from flask import jsonify
 
-# from flask_cors import CORS, cross_origin
+from flask_cors import CORS, cross_origin
 import body
 
 app = Flask(__name__, static_url_path="")
-# CORS(app)
+CORS(app)
 
 
-# def gen(camera):
-# while True:
-# frame = camera.get_frame()
-# yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-# @app.route('/video_feed')
-# def video_feed():
-# return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route("/<int:signal>")
