465b684a31f9d5e7ffa4df9f2d716494d48ed6c5
Aravind <akesiraju@users.noreply.github.com>
2022-04-10 15:39:31 -0400

    Avoid jerks and support hand rotation

--- mike/http_server.py
+++ mike/http_server.py
@@ -7,38 +7,58 @@ from flask import jsonify
 
 from flask_cors import CORS, cross_origin
 import body
+import time
 
 app = Flask(__name__, static_url_path="")
 CORS(app)
+
+sleep_time_for_smooth_movement = 0.5
 
 
 def gen(camera):
     while True:
         frame = camera.get_frame()
-        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n")
 
-@app.route('/video_feed')
+
+@app.route("/video_feed")
 def video_feed():
-    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen(VideoCamera()), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
 @app.route("/<int:signal>")
 def move(signal):
     if signal == 258:
         body.stop_walk()
+        time.sleep(sleep_time_for_smooth_movement)
         body.walk_forward()
 
     if signal == 260:
         body.stop_walk()
+        time.sleep(sleep_time_for_smooth_movement)
         body.turn_left()
 
     if signal == 261:
         body.stop_walk()
+        time.sleep(sleep_time_for_smooth_movement)
         body.turn_right()
 
     if signal == 259:
         body.stop_walk()
+        time.sleep(sleep_time_for_smooth_movement)
         body.walk_backward()
+
+    if signal == 301:
+        body.rotate_right_hand_clockwise()
+
+    if signal == 302:
+        body.rotate_right_hand_counter_clockwise()
+
+    if signal == 303:
+        body.rotate_left_hand_clockwise()
+
+    if signal == 304:
+        body.rotate_left_hand_counter_clockwise()
 
     if signal == 32 or signal == 27:
         body.stop_walk()
